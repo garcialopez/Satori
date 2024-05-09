c0ded6586dd78ce056242e8e8fc840bba8a82d34
codadry <recursosdeprogramacion2020@gmail.com>
2024-05-09 08:58:04 -0600

    Cambios

--- src/controlador/CAsistencia.java
+++ src/controlador/CAsistencia.java
@@ -1,21 +1,10 @@
 package controlador;
 
-import vista.VAsistencia;
 
 public class CAsistencia {
-    VAsistencia sistencia;
-
-    public CAsistencia(VAsistencia sistencia) {
-        this.sistencia = sistencia;
-    }
     
-    public void iniciarMaterias() {
-        this.sistencia.setLocationRelativeTo(null);
-        this.sistencia.setTitle("Asistencia");
-        this.sistencia.setResizable(false);
-        this.sistencia.pack();  //redimensión de contenedores
-        this.sistencia.setVisible(true);
-    }
+
+    
     
     
 }
